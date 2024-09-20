bf3e7fcce67f0861ea9fb3abde94166a8d3c13c5
Lucio Henrique <lucions96@gmail.com>
2024-09-20 01:30:04 -0300

    Refactor API endpoint for sending resume

--- src/infra/whtasapp/whatsapp.service.ts
+++ src/infra/whtasapp/whatsapp.service.ts
@@ -4,5 +4,5 @@ import { User } from "../database/models/user.model";
 const api = axios.create({ baseURL: process.env.RESUME_API_URL });
 
 export const postSendResume = async (user: User) => {
-  await api.post("/generateResume", user);
+  await api.post("/generate-resume", user);
 };
